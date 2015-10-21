09d5c5f6e584ce27e6cb0d746622b4232aa505b0
Brian Wagner <briwagner@outlook.com>
2015-10-20 22:35:57 -0400

    Finish two exercises

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,7 +1,14 @@
-puts "Hello, what's your name?"
-first_name = gets.chomp
-puts "Do you have a last name, " + first_name + "?"
-last_name = gets.chomp
-puts "Do you prefer " + first_name + ", or Mr. " + last_name + "?"
-used_name = gets.chomp
-puts "Ok, then. " + used_name + " it is."
+puts "Want to play a game?"
+reply1 = gets.chomp.downcase
+
+if reply1 == "yes"
+  puts "Hello, what's your name?"
+  first_name = gets.chomp
+  puts "Do you have a last name, " + first_name + "?"
+  last_name = gets.chomp
+  puts "Do you prefer " + first_name + ", or Mr. " + last_name + "?"
+  used_name = gets.chomp
+  puts "Ok, then. " + used_name + " it is."
+elsif reply1 == "no"
+  puts "Maybe another time"
+end
