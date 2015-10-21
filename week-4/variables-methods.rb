cd9f1163c39fbe1dadd74d5f2fd5fb216dd1cb55
Brian Wagner <briwagner@outlook.com>
2015-10-20 22:02:41 -0400

    Add variables-methods

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,7 @@
+puts "Hello, what's your name?"
+first_name = gets.chomp
+puts "Do you have a last name, " + first_name + "?"
+last_name = gets.chomp
+puts "Do you prefer " + first_name + ", or Mr. " + last_name + "?"
+used_name = gets.chomp
+puts "Ok, then. " + used_name + " it is."
