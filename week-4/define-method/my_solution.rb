fcb2c5f682bb7b74179c02fe396dcf80953d873a
Brian Wagner <briwagner@outlook.com>
2015-10-20 22:58:31 -0400

    Complete address challenge

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+def do_something(time, date, place)
+  puts "What is the " + time + "?"
+  puts "Today's date is " + date + "."
+  puts "Is " + place + "a happy place?"
+end
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
