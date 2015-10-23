0a41748a00f529c6b5ceee54763295b73df70568
Brian Wagner <briwagner@outlook.com>
2015-10-23 16:35:50 -0400

    Complete 4-5 and 4-7 pairs

---
+++ week-4/leap-years/bri_solution.rb
@@ -0,0 +1,15 @@
+# Leap Years
+
+# I worked on this challenge [Amaar].
+
+
+# Your Solution Below
+
+def leap_year?(our_year)
+  if our_year % 4 == 0 || (our_year % 100 == 0 && our_year % 400 == 0)
+    puts true
+  return true
+  end
+end
+
+leap_year?(2001)
