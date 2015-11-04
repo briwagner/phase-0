a292502d01c59201d7748774a80758531c17822e
Brian Wagner <briwagner@outlook.com>
2015-11-03 21:56:38 -0500

    Complete pair challenge credit card

--- week-6/validate-credit-card/my_solution.rb
+++ week-6/validate-credit-card/my_solution.rb
@@ -18,8 +18,41 @@
 
 class CreditCard
 
+  def initialize(number)
+    if number.to_s.length != 16
+      raise ArgumentError.new ("Must be 16 digits")
+    else
+      @number = number
+    end
+  end
+
+  def check_card
+    new_array = @number.to_s.split("")
+    new_array.each_with_index do |item, index|
+      if index % 2 == 0
+         new_array[index] = item.to_i * 2
+      else
+        new_array[index] = item.to_i
+      end
+    end
+    new_array.each_with_index do |item, index|
+      if item >= 10
+        new_array[index] = item - 10
+        new_array.push(1)
+      end
+    end
+    if new_array.reduce(:+) % 10 == 0
+      return true
+    else
+      return false
+    end
+  end
+
 end
 
+card = CreditCard.new(1266123412341234)
+
+p card.check_card
 
 
 # Refactored Solution
