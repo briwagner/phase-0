bcf169ecf83bec514d4f819eacb6e0226c8fa02d
Brian Wagner <briwagner@outlook.com>
2015-11-10 12:32:02 -0500

    Prepare group directory w/ Person 1

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,13 @@
+As a user, I want to use a function called sum that will add up all the numbers
+in a list, and give me the result.
+
+As a user, I want to use a function called mean that will calculate the average
+of all the numbers in a list.
+
+As a user, I want to use a function called median that will calculate the median
+of all the numbers in a list.
+For a list with an odd-number of entries, the median is the middle number.
+For a list with an even-number, the median is the average of the two numbers
+closest to the middle.
+
+
